7c34689481cee7330e1d007418b626c108aec1f3
Teanna Taylor <teannamtaylor95@gmail.com>
2022-06-20 14:23:52 -0700

    commit tree 6f8089db1315b92813ed306363f08cd48e7d6c6d

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//create totalBatteries - holds the sum of above array 
+
+const initialValue = 0;
+
+const totalBatteries = batteryBatches.reduce(
+
+    (previousValue,currentValue) => previousValue + currentValue, 
+    initialValue
+);
+
+console.log(totalBatteries);
+
